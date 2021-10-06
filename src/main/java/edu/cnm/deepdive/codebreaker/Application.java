8ecc574c4257ccdf6c9027d0f9bb8a95066e1c09
zcr003 <zramsey1@cnm.edu>
2021-10-06 17:10:28 -0600

    Implemented startGame round trip.

--- src/main/java/edu/cnm/deepdive/codebreaker/Application.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/Application.java
@@ -1,13 +1,44 @@
 package edu.cnm.deepdive.codebreaker;
 
+import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.service.WebServiceProxy;
+import java.io.IOException;
+import retrofit2.Call;
+import retrofit2.Response;
+
 public class Application {
 
-  public static void main(String[] args) {
-    // TODO Read command-line args for pool & length.
-    // TODO Make Request to codebreaker service to start game with a new code.
+  public Application() throws IOException {
+  }
+
+  public static void main(String[] args) throws IOException {
+
+  // TODO Read command-line args for pool & length.
+    String pool = "ABCDEF"; //FIXME Read from args
+    int length = 3; //FIXME Read from args
+    Game game = startGame (pool, length);
+    System.out.printf("Game id = %s%n", game.getId());
     // TODO While code is not guessed:
     //    1.Read guess from user input
     //    2.Submit guess to codebreaker service
     //    3. Display guess results
+
+  }
+
+
+
+
+  private static Game startGame(String pool, int length) throws IOException {
+    WebServiceProxy proxy = WebServiceProxy.getInstance();
+    Game game = new Game();
+    game.setPool(pool);
+    game.setLength(length);
+    Call<Game> call = proxy.startGame(game);
+    Response<Game> response = call.execute();
+    if (!response.isSuccessful()) {
+      throw new RuntimeException(response.message());
+    }
+      return response.body();
+
   }
 }
--- src/main/java/edu/cnm/deepdive/codebreaker/service/WebServiceProxy.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/service/WebServiceProxy.java
@@ -2,10 +2,29 @@ package edu.cnm.deepdive.codebreaker.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.model.Guess;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface WebServiceProxy {
+
+  @POST("codes")
+  Call<Game> startGame(@Body Game game);
+
+  @POST("codes/{gameId}/guesses")
+  Call<Guess> submitGuess(@Body Guess guess, @Path("gameId") String gameId);
+
+
+  static WebServiceProxy getInstance() {
+    return InstanceHolder.INSTANCE;
+
+  }
+
 
   class InstanceHolder {
 
